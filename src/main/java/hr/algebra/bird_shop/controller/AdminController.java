cbbf19df0d352611b894ba9b8d0ff73e26568878
Lolster24Vito <vitomir.hardi@gmail.com>
2024-02-05 00:21:55 +0100

    Added a seperate AdminController where he can do all of his admin stuff and removed admin stuff  from the regular home.html

--- src/main/java/hr/algebra/bird_shop/controller/AdminController.java
+++ src/main/java/hr/algebra/bird_shop/controller/AdminController.java
@@ -1,15 +1,19 @@
 package hr.algebra.bird_shop.controller;
 
+import hr.algebra.bird_shop.domain.Bird;
+import hr.algebra.bird_shop.domain.BirdTag;
 import hr.algebra.bird_shop.repository.BirdOrderRepository;
+import hr.algebra.bird_shop.repository.BirdRepository;
+import hr.algebra.bird_shop.repository.BirdTagRepository;
 import hr.algebra.bird_shop.repository.UserLoginInfoRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -18,13 +22,25 @@ import java.time.LocalDateTime;
 @RequestMapping("/admin")
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
+    private final BirdRepository birdRepository;
+    private final BirdTagRepository birdTagRepository;
     private final UserLoginInfoRepository userLoginInfoRepository;
     private final BirdOrderRepository birdOrderRepository;
+
+    @ModelAttribute
+    public void addAAttributeToModel(Model model) {
+        // Add the cookieCart attribute to the model for every request in this controller
+        model.addAttribute("createBird",new Bird());
+        model.addAttribute("birds",birdRepository.findAll());
+        model.addAttribute("birdTags",birdTagRepository.findAll());
+    }
     @GetMapping("/loginHistory")
     public String loginHistory(Model model){
         model.addAttribute("loginInfos",userLoginInfoRepository.findAll());
         return "loginHistory";
     }
+
+
     @GetMapping("/paymentHistory")
     public String paymentHistory(Model model,@RequestParam(value = "searchUser", required = false) String searchUser,
   @RequestParam(value = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
@@ -45,4 +61,45 @@ public class AdminController {
 
         return "paymentHistory";
     }
+    @GetMapping("/addTags")
+    public String addTags(Model model){
+        model.addAttribute("birdTags",birdTagRepository.findAll());
+        model.addAttribute("createTag",new BirdTag());
+        return "addTags";
+    }
+    @PostMapping("/saveNewTag")
+    public String saveNewTag(@Valid @ModelAttribute ("createTag") BirdTag createTag ,
+                             BindingResult bindingResult, Model model ){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("birdTags",birdTagRepository.findAll());
+            return "addTags";
+        }
+        else{
+            birdTagRepository.save(createTag);
+            return "redirect:/admin/addTags";
+        }
+
+    }
+    @GetMapping("/home")
+    public String adminHome(Model model){
+        return "adminHome";
+    }
+    private void addHomePageBirdAttributes(Model model){
+        model.addAttribute("birds",birdRepository.findAll());
+        model.addAttribute("birdTags",birdTagRepository.findAll());
+    }
+
+    @PostMapping("/saveNewBird")
+    public String saveNewBird(@Valid @ModelAttribute("createBird")  Bird createBird, BindingResult bindingResult,Model model){
+        if(bindingResult.hasErrors()){
+            addHomePageBirdAttributes(model);
+            return "adminHome";
+        }
+        else{
+            birdRepository.save(createBird);
+            return "redirect:/admin/home";
+        }
+
+    }
+
 }
--- src/main/java/hr/algebra/bird_shop/controller/BirdShopController.java
+++ src/main/java/hr/algebra/bird_shop/controller/BirdShopController.java
@@ -1,10 +1,8 @@
 package hr.algebra.bird_shop.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hr.algebra.bird_shop.domain.Bird;
 import hr.algebra.bird_shop.domain.BirdShopItem;
-import hr.algebra.bird_shop.domain.BirdTag;
 import hr.algebra.bird_shop.domain.Cart;
 import hr.algebra.bird_shop.repository.BirdRepository;
 import hr.algebra.bird_shop.repository.BirdTagRepository;
@@ -12,16 +10,12 @@ import hr.algebra.bird_shop.util.CookieCartUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 
 
@@ -35,11 +29,7 @@ public class BirdShopController {
     private final CookieCartUtil cookieCartUtil;
 
     @GetMapping({"/", "home"})
-    public String home(Authentication authentication,Model model, HttpServletRequest request){
-        //addHomePageBirdAttributes(model);
-       // model.addAttribute("createBird",new Bird());
-       // model.addAttribute("cookieCart",getCookieCart(request));
-
+    public String home(Authentication authentication,Model model){
         if (authentication != null && authentication.isAuthenticated()) {
             // Add the authentication object to the model if needed
             model.addAttribute("authentication", authentication);
@@ -55,45 +45,9 @@ public class BirdShopController {
         model.addAttribute("cookieCart",cookieCartUtil.getCookieCart(request));
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping("/saveNewBird")
-    public String saveNewBird(@Valid @ModelAttribute("createBird")  Bird createBird, BindingResult bindingResult,Model model){
-        if(bindingResult.hasErrors()){
-            addHomePageBirdAttributes(model);
-            return "home";
-        }
-        else{
-            birdRepository.save(createBird);
-            return "redirect:/";
-        }
 
-    }
-    private void addHomePageBirdAttributes(Model model){
-        model.addAttribute("birds",birdRepository.findAll());
-        model.addAttribute("birdTags",birdTagRepository.findAll());
-    }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping("addTags")
-    public String addTags(Model model){
-        model.addAttribute("birdTags",birdTagRepository.findAll());
-        model.addAttribute("createTag",new BirdTag());
-        return "addTags";
-    }
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping("/saveNewTag")
-    public String saveNewTag(@Valid @ModelAttribute ("createTag") BirdTag createTag ,
-                             BindingResult bindingResult,Model model ){
-        if(bindingResult.hasErrors()){
-            model.addAttribute("birdTags",birdTagRepository.findAll());
-            return "addTags";
-        }
-        else{
-            birdTagRepository.save(createTag);
-            return "redirect:/addTags";
-        }
 
-    }
     @PostMapping("/addBirdToCart/{id}")
     public String addBirdToCart(HttpServletResponse response,HttpServletRequest request, @RequestParam Integer quantity, @PathVariable Long id)
             throws JsonProcessingException {
